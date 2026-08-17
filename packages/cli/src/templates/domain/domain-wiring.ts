8ec0972f0b9eff97df86b118723c3a035d77f0f6
dogganidhal <nidhal.dogga@doctolib.com>
2026-08-18 00:50:02 +0200

    fix(cli): add required queuePrefix to rabbitmq wiring template
    Generated project scaffold failed tsc since RabbitMqEventBusConfig
    requires queuePrefix.

--- packages/cli/src/templates/domain/domain-wiring.ts
+++ packages/cli/src/templates/domain/domain-wiring.ts
@@ -167,6 +167,7 @@ const main = async () => {
       commandBus: new InMemoryCommandBus(),
       eventBus: new RabbitMqEventBus({
         url: "amqp://localhost:5672",
+        queuePrefix: "${ctx.name}",
       }),
       queryBus: new InMemoryQueryBus(),
     }),
